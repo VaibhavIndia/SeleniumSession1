5a138298108a321846f69b0594d09710e8895f75
VaibhavIndia <vaibhav.nigade@gmail.com>
2020-11-21 18:39:38 +0530

    chromium related changes added

--- seleniumsession_basic/BasicBrowserOperations.py
+++ seleniumsession_basic/BasicBrowserOperations.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.utils import ChromeType
+from webdriver_manager.firefox import GeckoDriverManager
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get('https://www.google.com')
 
+driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+#driver = webdriver.Firefox(GeckoDriverManager().install())
+driver.get("https://www.google.com")
+
+driver.maximize_window()
 print(driver.title)
 print(driver.current_url)
 print(driver.name)
--- seleniumsession_basic/CrossBrowserTesting.py
+++ seleniumsession_basic/CrossBrowserTesting.py
@@ -2,6 +2,7 @@ from selenium import  webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.utils import ChromeType
 
 browserName = "firefox"
 
@@ -9,9 +10,11 @@ if browserName == "chrome":
     driver = webdriver.Chrome(ChromeDriverManager().install())
 elif browserName == "firefox":
     driver = webdriver.Firefox(executable_path = GeckoDriverManager().install())
+elif browserName == "chromium":
+    driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 else:
     print("error in browsername")
 
 driver.maximize_window()
 driver.get("https://www.google.com")
-driver.find_element(By.ID,"q")
+driver.find_element(By.ID,"q").send_keys("hi")
