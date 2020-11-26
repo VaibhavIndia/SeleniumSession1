3a66e140bf347a78863d5be2b9ad209b679b4652
VaibhavIndia <vaibhav.nigade@gmail.com>
2020-11-26 21:54:50 +0530

    added new files 3

--- seleniumsession_basic/BasicBrowserOPen.py
+++ seleniumsession_basic/BasicBrowserOPen.py
@@ -3,3 +3,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get("https://www.google.com")
+driver.maximize_window()
+assert "Google1" in driver.title
+
+driver.find_element_by_name('q').send_keys("hi")
