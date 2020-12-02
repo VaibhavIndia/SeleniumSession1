ebf7217073703fc16b39dfeedd04fa6148201d79
VaibhavIndia <vaibhav.nigade@gmail.com>
2020-12-02 11:10:57 +0530

    added new files 3

---
+++ listPractice/List4.py
@@ -0,0 +1,26 @@
+for num in range(0,10):
+    if num % 2 == 0:
+        print(num)
+    else:
+        continue
+
+myList = []
+for num in range(1,50):
+    if num % 3 == 0:
+        myList.append(num)
+    else:
+        continue
+
+print(myList)
+
+for num in range (1,100):
+    if num % 3 == 0 and num % 5 == 0:
+        print(f" {num} FIZZBUZZ")
+    elif num % 3 == 0 and num % 5 != 0:
+        print(f"{num} Fizz")
+    elif num % 3 != 0 and num % 5 ==0:
+        print(f"{num} BUZZ")
+    else:
+        print(num)
+
+
